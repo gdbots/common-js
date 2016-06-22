496812b7b9a06b822a5c2cbd57af0ec5466c5d70
Michael H. Arieli <michaelharieli@gmail.com>
2016-06-22 08:26:32 -0700

    Added get_class function

--- src/common/util/system-utils.js
+++ src/common/util/system-utils.js
@@ -25,6 +25,39 @@ export default class SystemUtils
   }
 
   /**
+   * example 1: get_class(new (function MyClass() {}));
+   * returns 1: "MyClass"
+   *
+   * example 2: get_class({});
+   * returns 2: "Object"
+   *
+   * example 3: get_class([]);
+   * returns 3: false
+   *
+   * example 4: get_class(42);
+   * returns 4: false
+   *
+   * example 5: get_class(window);
+   * returns 5: false
+   *
+   * example 6: get_class(function MyFunction() {});
+   * returns 6: false
+   */
+  getClass(obj) {
+    if (obj && typeof obj === 'object' &&
+			Object.prototype.toString.call(obj) !== '[object Array]' &&
+			obj.constructor && obj !== this.window) {
+        var arr = obj.constructor.toString().match(/function\s*(\w+)/);
+
+        if (arr && arr.length === 2) {
+            return arr[1];
+        }
+    }
+
+    return false;
+  }
+
+  /**
    * Mixin traits into a class.
    *
    * ex: class Foo extends mixinClass(Trait1) {}
